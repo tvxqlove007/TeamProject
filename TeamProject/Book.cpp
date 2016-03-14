464fe035da2c86a1d30ec3a6a21698452a48874c
archanazone24 <archana.zone24@gmail.com>
2016-03-13 23:26:58 -0700

    Update Book.cpp

--- TeamProject/Book.cpp
+++ TeamProject/Book.cpp
@@ -6,15 +6,99 @@
 
 Book::Book(string isbn, string title, string author, string publisher, 
 			time_t dateAdded, int quantity, double wholesaleCost, double retailPrice) {
-	this->isbn = isbn;
-	this->title = title;
-	this->author = author;
-	this->publisher = publisher;
-	this->dateAdded = dateAdded;
-	this->quantityOnHand = quantity;
-	this->wholesaleCost = wholesaleCost;
-	this->retailPrice = retailPrice;
+	this->setIsbn(isbn);
+	this->setTitle(title);
+	this->setAuthor(author);
+	this->setPublisher(publisher);
+	this->setDateAdded(dateAdded);
+	this->setQuantityOnHand(quantity);
+	this->setWholesaleCost(wholesaleCost);
+	this->setRetailPrice(retailPrice);
 }
+
+void Book::setIsbn(string i) { 
+	isbn = i; 
+}
+
+void Book::setTitle(string t) {
+	if (t == "")
+	{
+		throw EmptyTitle();
+	}
+	else
+	{
+		title = t;
+	}
+}
+
+void Book::setAuthor(string a) { 
+	if (a == "")
+	{
+		throw EmptyAuthor();
+	}
+	else
+	{
+		author = a;
+	}
+}
+
+void Book::setPublisher(string p) {
+	if (p == "")
+	{
+		throw EmptyPublisher();
+	}
+	else
+	{
+		publisher = p;
+	}
+}
+
+void Book::setDateAdded(time_t d) { 
+	dateAdded = d; 
+}
+
+void Book::setQuantityOnHand(int q) {
+	if (q <=0)
+	{
+		throw NonPositiveQuantity(q);
+	}
+	else
+	{
+		quantityOnHand = q;
+	}
+
+}
+
+void Book::setWholesaleCost(double w) {
+	if (w <= 0)
+	{
+		throw NonPositiveWholesalecost(w);
+	}
+	else
+	{
+		wholesaleCost = w;
+	}
+}
+
+void Book::setRetailPrice(double r) {
+	if (r <= 0)
+	{
+		throw NonPositiveRetailprice(r);
+	}
+	else
+	{
+		retailPrice = r;
+	}
+}
+
+string Book::getIsbn() { return isbn; }
+string Book::getTitle() { return title; }
+string Book::getAuthor() { return author; }
+string Book::getPublisher() { return publisher; }
+time_t Book::getDateAdded() { return dateAdded; }
+int Book::getQuantityOnHand() { return quantityOnHand; }
+double Book::getWholesaleCost() { return wholesaleCost; }
+double Book::getRetailPrice() { return retailPrice; }
 
 Book::Book() {
 	isbn = "";
