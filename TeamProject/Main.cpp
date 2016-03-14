1cd7c398f622c23320bf38a3afa46d12d7e1601b
archanazone24 <archana.zone24@gmail.com>
2016-03-13 23:30:33 -0700

    Update Main.cpp

--- TeamProject/Main.cpp
+++ TeamProject/Main.cpp
@@ -2,11 +2,14 @@
 #include <iostream>
 
 #include "Module.h"
+#include "BookDAO.h"
 
 using namespace std;
 
 int main()
 {
+	// To initialize the DAO object at start up
+	BookDAO::getInstance();
 	Module::showMainMenu();
 	return 0;
 }
